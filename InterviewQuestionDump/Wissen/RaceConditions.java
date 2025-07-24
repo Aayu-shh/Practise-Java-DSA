694484069a603ba01dc76292b721c4f4b9fbcff5
Aayu-shh <aayush28.official@gmail.com>
2025-07-25 00:56:47 +0530

    Practise Multithreading Atomic Integer

--- InterviewQuestionDump/Wissen/RaceConditions.java
+++ InterviewQuestionDump/Wissen/RaceConditions.java
@@ -1,5 +1,7 @@
 package InterviewQuestionDump.Wissen;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class RaceConditions {
     public static void main(String[] args) throws InterruptedException {
         Counter counter = new Counter();
@@ -15,13 +17,13 @@ public class RaceConditions {
         t1.join(); // To Ensure Control goes to main POST execution completion of t1
         t2.join(); // and of t2
         System.out.println("In the Thread: " + Thread.currentThread().getName());  //Main thread
-        System.out.println("Expected 2000, Actual is : "+counter.counter); // Fixed using synchronized over .increment() method
+        System.out.println("Expected 2000, Actual is : "+counter.counter.get()); // Fixed using counter -> AtomicInteger & .increment() method > incrementAndGet()
     }
 }
 
 class Counter{
-    int counter=0;
-    synchronized void increment(){
-        counter++;
+    AtomicInteger counter=new AtomicInteger(0);
+    void increment(){
+        counter.incrementAndGet();
     }
 }
